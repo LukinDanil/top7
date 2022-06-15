b9edc580dc2f7b92c4bea03634794e3752f571cf
Danil Lukin <danillukin161100@mail.ru>
2022-06-15 12:13:21 +0300

    scrollSection (Якорная система)

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,61 +2,77 @@
 
 let scrolling = false;
 let duration = 0;
+let screen = 0;
 
-function alignScrollSection() {
-    $('.section').each(function () {
-        if ($(this).offset().top <= $(document).scrollTop() && ($(this).offset().top + ($(this).height() / 2)) >= $(document).scrollTop()) {
-            // if (!scrolling) {
-                // scrolling = true;
-                let scrollAlign = $(this).offset().top;
-                $('html, body').stop().animate({
-                    scrollTop: scrollAlign
-                }, duration);
-                console.log('ALIGN = ' + scrollAlign);
-                // setTimeout(function () { scrolling = false }, 500);
-            // }
-        }
+function sectionsInit() {
+    $('.section').each(function (index) {
+        $(this).attr('data-screen-index', index);
+    });
+
+    if (!scrolling) {
+        scrolling = true;
+        let html = $('html, body');
+        let scrollTo = $(`[data-screen-index='${screen}']`).offset().top;
+        clearActiveScreen();
+        $(`[data-screen-index='${screen}']`).addClass('active');
+        html.stop().animate({ scrollTop: scrollTo }, duration);
+        setTimeout(function () { scrolling = false; }, 500);
+    }
+};
+
+function clearActiveScreen() {
+    $(`[data-screen-index]`).each(function () {
+        $(this).removeAttr('active');
     });
 }
 
 function sectionScrollDown() {
     if (!scrolling) {
         scrolling = true;
+        if ($('[data-screen-index]').length - 1 > screen) {screen++}
         let html = $('html, body');
-        let scrollTo = $(document).scrollTop() + $(window).height();
-        html.stop().animate({ scrollTop: scrollTo }, duration);
-        console.log('DOWN = ' + scrollTo);
-        setTimeout(function () { scrolling = false; alignScrollSection() }, 500);
+        let scrollTo = $(`[data-screen-index='${screen}']`).offset().top;
+        clearActiveScreen();
+        $(`[data-screen-index='${screen}']`).addClass('active');
+        html.queue(function(){
+            $(this).animate({ scrollTop: scrollTo }, duration).dequeue();
+        });
+        console.log('screen = ' + screen);
+        setTimeout(function () { scrolling = false; }, 500);
     }
 }
 
 function sectionScrollUp() {
     if (!scrolling) {
         scrolling = true;
+        if (screen > 0) {screen--}
         let html = $('html, body');
-        let scrollTo = $(document).scrollTop() - $(window).height();
-        html.stop().animate({ scrollTop: scrollTo }, duration);
-        console.log('UP = ' + scrollTo);
-        setTimeout(function () { scrolling = false; alignScrollSection() }, 500);
+        let scrollTo = $(`[data-screen-index='${screen}']`).offset().top;
+        clearActiveScreen();
+        $(`[data-screen-index='${screen}']`).addClass('active');
+        html.queue(function(){
+            $(this).animate({ scrollTop: scrollTo }, duration).dequeue();
+        });
+        console.log('screen = ' + screen);
+        setTimeout(function () { scrolling = false; }, 500);
     }
 }
 
 function sectionScroll() {
     $(document).bind('DOMMouseScroll.customDOMMouseScroll mousewheel.customMousewheel', function (e) {
-        // e.preventDefault();
         if (e.originalEvent.wheelDelta / 120 > 0) {
             sectionScrollUp(e);
         }
         else {
             sectionScrollDown(e);
         }
-        // console.log('sectionScroll');
     });
 }
 
 (function ($) {
     $(document).ready(function () {
-        alignScrollSection();
+        sectionsInit();
+        // alignScrollSection();
         $('.section__nav-arrows .section__prev').click(sectionScrollUp);
         $('.section__nav-arrows .section__next').click(sectionScrollDown);
         sectionScroll();
